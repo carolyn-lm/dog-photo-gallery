2bb71063baa9c481a78672d51f0cc264ce4e4f5b
carolyn_m <carolynlmadden@gmail.com>
2025-05-05 13:24:23 -0400

    Retrieving Dog Data with a Dictionary

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import requests
 
 #imports a dictionary of data from dog_breeds.py and "prettifies", or styles, the dog names when they appear in the HTML page
 from dog_breeds import prettify_dog_breed
@@ -15,7 +15,14 @@ def check_breed(breed):
 def dog_image_gallery():
   errors = []
   if request.method == "POST":
-     breed = request.form.get("breed");
+    breed = request.form.get("breed")
+    if not breed:
+      errors.append("Oops! Please choose a breed.")
+    if breed:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
+      data = response.json()
+      dog_images = data["message"]
+     
   return render_template("dogs.html")
 
 app.debug = True
